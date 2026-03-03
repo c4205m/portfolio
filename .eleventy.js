cfcf4a8b8e32aee63c80d21f34163016532acf0e
c4205M <ylmzcanmstf@gmail.com>
2026-03-03 23:39:29 +0300

    eleventy.js added

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
+export default function (eleventyConfig) {
+    eleventyConfig.addPlugin(HtmlBasePlugin);
+
+    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/header.css");
+    eleventyConfig.addPassthroughCopy("./src/footer.css");
+    eleventyConfig.addPassthroughCopy("./src/gallery.css");
+    eleventyConfig.addPassthroughCopy("./src/contact.css");
+
+    eleventyConfig.addPassthroughCopy("./src/app.js");
+    eleventyConfig.addPassthroughCopy("./src/contact.js");
+
+    eleventyConfig.addPassthroughCopy("./src/media");
+    
+    return {
+        dir: {
+            input: "src",
+            output: "_site",
+        },
+        pathPrefix: "/portfolio/",
+    };
+};
