7822ff7b7d295235def22f09cdc4136e3478448e
c4205m <c4205m@users.noreply.github.com>
2026-03-04 08:20:23 +0000

    deploy: 0ba95030ccf09c36211a74e434a0eeb82b837fb2

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const observer = new IntersectionObserver((els) => {
+const observer = new IntersectionObserver(observerAction, {
+    root: null,
+    rootMargin: "-17% 0px 0px 0px",
+    threshold: 0
+});
+
+const observer2 = new IntersectionObserver(observerAction, {
+    root: null,
+    rootMargin: "-17% 0px -50% 0px",
+    threshold: 1
+});
+
+let observeTargets = [];
+// observeTargets = [...observeTargets, ...document.querySelectorAll("li")];
+observeTargets = [...observeTargets, ...document.querySelectorAll("section")];
+observeTargets = [...observeTargets, ...document.querySelectorAll(".img-title")];
+observeTargets = [...observeTargets, ...document.querySelectorAll(".gallery-item")];
+observeTargets = [...observeTargets, ...document.querySelectorAll("video")];
+observeTargets.forEach(e => observer.observe(e));
+
+observeTargets = [];
+observeTargets = [...observeTargets, ...document.querySelectorAll(".linkemph")];
+observeTargets.forEach(e => observer2.observe(e));
+
+function observerAction(els){
     els.forEach((el) => {
         if(el.isIntersecting){
             if(el.target.tagName === "VIDEO"){
@@ -11,16 +35,4 @@ const observer = new IntersectionObserver((els) => {
             el.target.classList.remove("show");
         }
     })
-}, {
-    root: null,
-    rootMargin: "-300px 0px 0px 0px",
-    threshold: 0
-});
-
-let observeTargets = [];
-// observeTargets = [...observeTargets, ...document.querySelectorAll("li")];
-observeTargets = [...observeTargets, ...document.querySelectorAll("section")];
-observeTargets = [...observeTargets, ...document.querySelectorAll(".img-title")];
-observeTargets = [...observeTargets, ...document.querySelectorAll(".gallery-item")];
-observeTargets = [...observeTargets, ...document.querySelectorAll("video")];
-observeTargets.forEach(e => observer.observe(e));
+}
