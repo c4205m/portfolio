03a2871a1b86acf97c32c35f347e54de98c72379
c4205M <ylmzcanmstf@gmail.com>
2026-06-23 20:18:35 +0300

    fix: reset scroll to top on route change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { useLinkEmphasis } from "../hooks/useLinkEmphasis";
 
 export function Layout() {
+  const { pathname } = useLocation();
   useLinkEmphasis();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <>
